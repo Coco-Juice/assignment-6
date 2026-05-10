91e920a983c9eccfd28a2587a6c3593ab912bc2c
Tony Bai <tonybai321@gmail.com>
2026-05-10 16:49:50 -0700

    created nldate and tests folders

---
+++ src/nldate/__init__.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+def parse(s: str, today: date | None = None) -> date:
+    """
+    Given a natural language date, return a datetime.date object representing the date that the input 
+    string refers to. The today parameter is used as a reference point or relative date expressions. If not
+    provided, default to current date
+    """
+
+def main() -> None:
+    print("hello")
